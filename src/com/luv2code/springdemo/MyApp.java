8ed0231702f9d1bef8d30b0c2e4bf918e4dc5b78
MANNE DEDEEPYA <dedeepyamanne02@gmail.com>
2022-01-26 01:28:47 +0530

    first commit

---
+++ src/com/luv2code/springdemo/MyApp.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		
+		//BaseballCoach theCoach = new BasballCoach();
+		//Coach theCoach = new BaseballCoach();
+		
+		Coach theCoach = new TrackCoach();
+		
+		System.out.println(theCoach.getDailyWorkout());
+
+	}
+
+}
